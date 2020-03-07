32b86a1c5cb8694dbf97b04e754029b8a36edd2f
Jihyun Yu <yjh0502@gmail.com>
2020-03-07 21:07:51 +0900

    add Stats

--- src/lib.rs
+++ src/lib.rs
@@ -279,7 +279,81 @@ pub fn bench_zip(input_filepath: &str, parallel: bool) -> io::Result<()> {
     Ok(())
 }
 
-pub fn calculate_depth(idx: usize, blobs: &[Blob], depths: &mut [usize]) -> usize {
+#[derive(Default)]
+struct Stats {
+    root_count: usize,
+    root_total_size: u64,
+
+    non_root_count: usize,
+    non_root_store_size: u64,
+    non_root_content_size: u64,
+}
+
+impl Stats {
+    fn from_blobs(blobs: &[Blob]) -> Self {
+        let mut stats = Stats::default();
+
+        for blob in blobs {
+            stats.add_blob(blob);
+        }
+        stats
+    }
+
+    fn add_blob(&mut self, blob: &Blob) {
+        match &blob.parent_hash {
+            None => {
+                self.root_count += 1;
+                self.root_total_size += blob.content_size;
+            }
+            Some(_parent_hash) => {
+                self.non_root_count += 1;
+                self.non_root_store_size += blob.store_size;
+                self.non_root_content_size += blob.content_size;
+            }
+        }
+    }
+
+    fn size_info(&self) -> String {
+        use bytesize::ByteSize;
+        use std::fmt::Write;
+
+        let mut s = String::new();
+        writeln!(
+            s,
+            "total count={}, size={}",
+            self.root_count + self.non_root_count,
+            ByteSize(self.root_total_size + self.non_root_store_size)
+        )
+        .ok();
+
+        writeln!(
+            s,
+            "root count={}, size={}, avg={}",
+            self.root_count,
+            ByteSize(self.root_total_size),
+            ByteSize(self.root_total_size / self.root_count as u64)
+        )
+        .ok();
+
+        let compression_ratio =
+            (self.non_root_store_size as f32) * 100.0 / (self.non_root_content_size as f32);
+
+        writeln!(
+            s,
+            "non_root count={}, store_size={}, content_size={}, compression={:.2}% ({:.2}x)",
+            self.non_root_count,
+            ByteSize(self.non_root_store_size),
+            ByteSize(self.non_root_content_size),
+            compression_ratio,
+            100.0 / compression_ratio
+        )
+        .ok();
+
+        s
+    }
+}
+
+fn calculate_depth(idx: usize, blobs: &[Blob], depths: &mut [usize]) -> usize {
     let blob = &blobs[idx];
 
     match blob.parent_hash {
@@ -315,55 +389,12 @@ pub fn calculate_depth(idx: usize, blobs: &[Blob], depths: &mut [usize]) -> usiz
 }
 
 pub fn debug_stats() -> io::Result<()> {
-    use bytesize::ByteSize;
     let blobs = db::all().expect("db::all");
 
     {
-        let mut root_count = 0;
-        let mut root_total_size = 0u64;
+        let mut stats = Stats::from_blobs(&blobs);
 
-        let mut non_root_count = 0;
-        let mut non_root_store_size = 0u64;
-        let mut non_root_content_size = 0u64;
-
-        for blob in &blobs {
-            match &blob.parent_hash {
-                None => {
-                    root_count += 1;
-                    root_total_size += blob.content_size;
-                }
-                Some(_parent_hash) => {
-                    non_root_count += 1;
-                    non_root_store_size += blob.store_size;
-                    non_root_content_size += blob.content_size;
-                }
-            }
-        }
-
-        println!(
-            "total count={}, size={}",
-            root_count + non_root_count,
-            ByteSize(root_total_size + non_root_store_size)
-        );
-
-        println!(
-            "root count={}, size={}, avg={}",
-            root_count,
-            ByteSize(root_total_size),
-            ByteSize(root_total_size / root_count as u64)
-        );
-
-        let compression_ratio =
-            (non_root_store_size as f32) * 100.0 / (non_root_content_size as f32);
-
-        println!(
-            "non_root count={}, store_size={}, content_size={}, compression={:.2}% ({:.2}x)",
-            non_root_count,
-            ByteSize(non_root_store_size),
-            ByteSize(non_root_content_size),
-            compression_ratio,
-            100.0 / compression_ratio
-        );
+        info!("{}", stats.size_info());
     }
 
     {
